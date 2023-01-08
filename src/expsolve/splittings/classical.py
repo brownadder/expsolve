32220b38b7154ed21aab3dff6fb3626d957a66de
Pranav Singh <singh.pranav@gmail.com>
2023-01-08 14:00:29 +0000

    classical splittings symmetric consistent

--- src/expsolve/splittings/classical.py
+++ src/expsolve/splittings/classical.py
@@ -6,23 +6,42 @@ def stepper(h, u0, expAu, expBu, alpha, beta):
     u = u0
     assert(len(alpha) == len(beta))
     for k in range(len(alpha)):
-        u = expAu(h*alpha[k], u)
-        u = expBu(h*beta[k], u)
+        if abs(alpha[k]) > 1e-14:
+            u = expAu(h*alpha[k], u)
+        if abs(beta[k]) > 1e-14:
+            u = expBu(h*beta[k], u)
     return u
 
 
 # alpha beta assumed underspecified by exactly 1 parameter
 def consistent(a, b):
-    alpha = a+[1.-sum(a)]
-    beta = b+[1.-sum(b)]
+    x = list(a)
+    y = list(b)
+    alpha = x+[1.-sum(x)]
+    beta = y+[1.-sum(y)]
     return alpha, beta
 
 
-# assume expAu outside and expBu inside
-# assume symmetric always needs to be consistent
+# expAu should be the first action
+# len(a)=len(b) or len(a)=len(b)+1
+# assuming that symmetric always needs to be consistent
 def symmetric(a, b):
-    amid = 1.-2.*sum(a)
-    bmid = 0.5-sum(b)
-    alpha = a + [amid] + a[::-1]
-    beta = b + [bmid, bmid] + b[::-1] + [0]
+    x = list(a)
+    y = list(b)
+    nx = len(x)
+    ny = len(y)
+    assert (nx == ny) or (nx == ny + 1)
+
+    xmid = 1.-2.*sum(x)
+    ymid = 0.5-sum(y)
+    X = x + [xmid] + x[::-1]
+    Y = y + [ymid, ymid] + y[::-1]
+    
+    if nx == ny:
+        alpha = Y
+        beta = X + [0]
+    else:
+        alpha = X
+        beta = Y + [0]
+
     return alpha, beta
